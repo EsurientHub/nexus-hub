beffda5d44d4e89a0a522fb132c2ecc19f5aa9d3
FrengkyVerve <frengky.sinaga@yourpay.co.id>
2025-09-02 22:35:55 +0700

    feat(project): init nexus project

---
+++ composeApp/src/jvmMain/kotlin/com/esurienthub/nexus/theme/Theme.jvm.kt
@@ -0,0 +1,7 @@
+package com.esurienthub.nexus.theme
+
+import androidx.compose.runtime.Composable
+
+@Composable
+internal actual fun SystemAppearance(isDark: Boolean) {
+}
---
+++ composeApp/src/jvmMain/kotlin/main.kt
@@ -0,0 +1,18 @@
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
+import com.esurienthub.nexus.App
+
+fun main() = application {
+    Window(
+        title = "Nexus Hub",
+        state = rememberWindowState(width = 800.dp, height = 600.dp),
+        onCloseRequest = ::exitApplication,
+    ) {
+        window.minimumSize = Dimension(350, 600)
+        App()
+    }
+}
+
